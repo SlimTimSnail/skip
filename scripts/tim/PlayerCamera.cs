20b856bb8859608273cc5f5782d9a439a4cbe8dd
TimDonnan <t.donnan@live.com.au>
2026-05-03 11:37:58 +1200

    camera script functional

--- scripts/tim/PlayerCamera.cs
+++ scripts/tim/PlayerCamera.cs
@@ -9,4 +9,22 @@ public partial class PlayerCamera : Camera2D
 
     [Export]
     private float _yOffset = 700f;
+
+    public override void _Ready()
+    {
+        SetStartingPosition();
+    }
+
+    public override void _Process(double delta)
+    {
+        Vector2 currentPosition = new Vector2(_playerNode.GlobalPosition.X, _yOffset);
+        SetPosition(currentPosition);
+    }
+
+    private void SetStartingPosition()
+    {
+        Vector2 startPosition = new Vector2(_playerNode.GlobalPosition.X, _yOffset);
+        SetPosition(startPosition);
+        ResetSmoothing();
+    }
 }
