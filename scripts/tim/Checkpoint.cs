0a03b1158ae29a56dc91f1d82baed28ff43d82cd
TimDonnan <t.donnan@live.com.au>
2026-05-03 12:46:08 +1200

    checkpoint script

---
+++ scripts/tim/Checkpoint.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class Checkpoint : Area2D
+{
+    [Export]
+    private bool _isFinalCheckpoint = false;
+
+    public override void _Ready()
+    {
+        BodyEntered += OnOverlap;
+    }
+
+    private void OnOverlap(Node2D body)
+    {
+        if (body is SquareChar player)
+        {
+            player.CheckpointOverlapped(_isFinalCheckpoint);
+        }
+    }
+
+}
