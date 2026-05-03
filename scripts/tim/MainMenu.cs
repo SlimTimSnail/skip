9f5f164d721f5003ec7cd7975474abeb68f36616
TimDonnan <t.donnan@live.com.au>
2026-05-03 16:42:49 +1200

    added how to play

--- scripts/tim/MainMenu.cs
+++ scripts/tim/MainMenu.cs
@@ -7,13 +7,45 @@ public partial class MainMenu : Node
     private Button _playButton;
 
     [Export]
+    private Button _howToPlayButton; 
+
+    [Export]
     private Button _quitButton; 
+
+    [Export]
+    private Button _returnButton; 
+
+    [Export]
+    private Control _howToPlayParent;
+
+    [Export]
+    private Control _menuParent;
+
 
     public override void _Ready()
     {
         _playButton.Pressed += OnPlayButtonPressed;
         _quitButton.Pressed += OnQuitButtonPressed;
+        _howToPlayButton.Pressed += OnHTPButtonPressed;
+        _returnButton.Pressed += OnReturnButtonPressed;
+        _howToPlayParent.Visible = false;
     }
+
+    private void OnReturnButtonPressed()
+    {
+        _howToPlayParent.Visible = false;
+        _menuParent.Visible = true;
+        _howToPlayButton.GrabFocus();
+    }
+
+
+    private void OnHTPButtonPressed()
+    {
+        _menuParent.Visible = false;
+        _howToPlayParent.Visible = true;
+        _returnButton.GrabFocus();
+    }
+
 
     private void OnPlayButtonPressed()
     {
