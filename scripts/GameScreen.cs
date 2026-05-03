8660f4f742aa9add274b8286462f20e2e2f7ade4
Sami Habib <puggsoy@gmail.com>
2026-05-03 12:44:05 +1200

    Changed PlayerLose signal to an event

--- scripts/GameScreen.cs
+++ scripts/GameScreen.cs
@@ -2,6 +2,14 @@ using Godot;
 
 public partial class GameScreen : Node
 {
+	[Export]
+	private SquareChar player = null;
+
+	public override void _Ready()
+	{
+		player.PlayerLose += GoToEnd;
+	}
+
 	public void GoToEnd()
 	{
 		FlowManager.Instance.ChangeState(FlowManager.GameState.LoseMenu);
